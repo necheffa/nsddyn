fd9e4ffe34f9acf9a1a9436d14783c2de7ca1347
Alexander Necheff <necheffa@necheff.net>
2019-12-15 15:24:22 -0500

    relocating some sample scripts

--- scripts/trivial_curl_client02.sh
+++ scripts/trivial_curl_client02.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-curl -X POST -H "Content-Type: application/json" http://127.0.0.1:5000/api/dynupd -d '{"username": "alex", "password": "yeet", "ipaddr": "127.0.0.100", "hostname": "myhost"}'
+curl -X POST -H "Content-Type: application/json" http://127.0.0.1:5000/api/dynupd -d \
+    '{"username": "alex", "password": "yeet", "ipaddr": "127.0.0.100", "hostname": "myhost"}'
 
