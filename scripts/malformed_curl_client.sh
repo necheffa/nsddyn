72ecf50e4aca9d89aae1a98908bb7481e29fcba4
Alexander Necheff <necheffa@necheff.net>
2020-02-07 10:49:18 -0500

    rewrite from Python to Go

--- scripts/malformed_curl_client.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-curl -X POST -H "Content-Type: application/json" http://127.0.0.1:5000/api/dynupd -d '{"name": "yeet"}'
-
--- scripts/testexec.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python3-coverage run -m pytest
-python3-coverage report
-python3-coverage html
