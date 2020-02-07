18b97b7294d5d07908a4a47b011d5b90cdd8b7f1
Alexander Necheff <necheffa@necheff.net>
2020-02-07 15:49:57 -0500

    adding skeleton build system, loosely based on Docker-ce

---
+++ src/cmd/internal/version/version.go
@@ -0,0 +1,28 @@
+/*
+   Copyright (C) 2019, 2020 Alexander Necheff
+
+   This file is part of nsddyn.
+
+   nsddyn is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, at version 3 of the License.
+
+   nsddyn is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with nsddyn.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// Package version provides versioning information.
+package version
+
+// ldflags are used to set these.
+var (
+	PlatformName = ""
+	Version      = "unknown-version"
+	GitCommit    = "unknown-commit"
+	BuildTime    = "unknown-buildtime"
+)
---
+++ src/cmd/nsddynum/main.go
@@ -0,0 +1,31 @@
+/*
+   Copyright (C) 2019, 2020 Alexander Necheff
+
+   This file is part of nsddyn.
+
+   nsddyn is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, at version 3 of the License.
+
+   nsddyn is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with nsddyn.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+package main
+
+import (
+	"fmt"
+
+	"cmd/internal/version"
+)
+
+func main() {
+	fmt.Println(version.Version)
+	fmt.Println(version.GitCommit)
+	fmt.Println(version.BuildTime)
+}
