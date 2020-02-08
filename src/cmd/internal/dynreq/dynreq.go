0b38f292492464b2fc29c7c595b3391c476da292
Alexander Necheff <necheffa@necheff.net>
2020-02-08 15:20:00 -0500

    created the DynReq struct

---
+++ src/cmd/internal/dynreq/dynreq.go
@@ -0,0 +1,27 @@
+/*
+   Copyright (C) 2019, 2020 Alexander Necheff
+
+   This file is part of nsddyn.
+
+   nsddyn is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, at version 3 of the License.
+
+   nsddyn is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with nsddyn.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+// Package dynreq provides the DynReq type.
+package dynreq
+
+type DynReq struct {
+	Username  string
+	Password  string
+	Ipaddr    string
+	Hostnames string
+}
