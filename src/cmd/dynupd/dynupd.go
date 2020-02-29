f6c61567a744f47c20eb8d1e78bcfeefaa79a4b3
Alexander Necheff <necheffa@necheff.net>
2020-02-29 17:48:08 -0500

    added capability to update zonefile, currently any request is applied without authentication

--- src/cmd/dynupd/dynupd.go
+++ src/cmd/dynupd/dynupd.go
@@ -19,15 +19,26 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 
 	"cmd/internal/config"
 	"cmd/internal/dynreq"
-	//"github.com/bwesterb/go-zonefile"
+	"cmd/internal/version"
+
+	"github.com/bwesterb/go-zonefile"
+)
+
+// DynUpd status codes - similar to HTTP status codes,
+// but not exactly the same.
+const (
+	zoneUpdateFail    = "500"
+	zoneUpdateSuccess = "200"
 )
 
 type DynUpd struct {
@@ -38,6 +49,100 @@ type DynUpd struct {
 func (d *DynUpd) NewDynUpd(passwdFile string, zoneFile string) {
 	d.passwdFile = passwdFile
 	d.zoneFile = zoneFile
+}
+
+// UpdateZone updates the zonefile with the specified request.
+// Returns a status code as a string to be sent to the requesting client.
+func (d *DynUpd) UpdateZone(r dynreq.DynReq) string {
+	file, err := os.OpenFile(d.zoneFile, os.O_RDWR, 0664)
+	if err != nil {
+		if config.Debug {
+			fmt.Fprintf(os.Stderr, "dynupd: error: failed to open zonefile for update: "+d.zoneFile)
+		}
+		return zoneUpdateFail
+	}
+	defer file.Close()
+
+	buf, err := ioutil.ReadAll(file)
+	if err != nil {
+		if config.Debug {
+			fmt.Fprintf(os.Stderr, "dynupd: error: failed to read zonefile for update: "+d.zoneFile)
+		}
+		return zoneUpdateFail
+	}
+
+	zf, err := zonefile.Load(buf)
+	if err != nil {
+		if config.Debug {
+			fmt.Fprintf(os.Stderr, "dynupd: error: failed to parse zonefile for update: "+d.zoneFile)
+		}
+		return zoneUpdateFail
+	}
+
+	updateSerial := false
+	for i := 0; i < len(r.Hostnames); i++ {
+		// find r.Hostnames[i] in the zonefile
+		foundHost := false
+		for _, e := range zf.Entries() {
+			if !bytes.Equal(e.Type(), []byte("A")) {
+				continue
+			}
+
+			if !bytes.Equal(e.Domain(), []byte(r.Hostnames[i])) {
+				continue
+			}
+
+			// we found a match, update it
+			e.SetValue(0, []byte(r.Ipaddr))
+			foundHost = true
+			updateSerial = true
+			break
+		}
+
+		// if it doesn't exist, add it
+		if !foundHost {
+			e, _ := zonefile.ParseEntry([]byte(r.Hostnames[i] + " IN A " + r.Ipaddr))
+			zf.AddEntry(e)
+			updateSerial = true
+		}
+	}
+
+	// does zonefile automatically update the serial? if not update it here.
+	if updateSerial {
+		for _, e := range zf.Entries() {
+			if !bytes.Equal(e.Type(), []byte("SOA")) {
+				continue
+			}
+
+			vals := e.Values()
+			if len(vals) != 7 {
+				if config.Debug {
+					fmt.Fprintf(os.Stderr, "dynupd: error: malformed SOA in zonefile\n")
+				}
+				return zoneUpdateFail
+			}
+
+			serial, _ := strconv.Atoi(string(vals[2]))
+			e.SetValue(2, []byte(strconv.Itoa(serial+1)))
+			break
+		}
+	} else {
+		if config.Debug {
+			fmt.Fprintf(os.Stderr, "dynupd: error: failed to update A record\n")
+			return zoneUpdateFail
+		}
+	}
+
+	// if we made it this far...things have gone well.
+
+	newZoneFile := zf.Save()
+	//TODO: handle possible write error getting returned here...
+	file.Write(newZoneFile)
+
+	//TODO: actually reload the zone here
+	fmt.Fprintf(os.Stderr, "dynupd: info: need to ask NSD to reload the zone here...\n")
+
+	return zoneUpdateSuccess
 }
 
 func (d *DynUpd) DynUpdHandler(w http.ResponseWriter, r *http.Request) {
@@ -80,5 +185,9 @@ func (d *DynUpd) DynUpdHandler(w http.ResponseWriter, r *http.Request) {
 		if config.Debug {
 			fmt.Fprintf(os.Stderr, "authentication successful, updating: "+d.zoneFile)
 		}
+
+		status := d.UpdateZone(msg)
+		retObj := "{ \"version\": " + version.Version + ", \"code\": " + status + " }"
+		fmt.Fprintf(w, retObj)
 	}
 }
