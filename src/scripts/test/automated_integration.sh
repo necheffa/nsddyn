0431437625ed71704eb08b639c1664a2b02a13d9
Alexander Necheff <necheffa@necheff.net>
2020-07-02 15:13:00 -0400

    change to use 192.0.2.0/24 address instead of 127.0.0.0/8. fixed issue #47

--- src/scripts/test/automated_integration.sh
+++ src/scripts/test/automated_integration.sh
@@ -7,7 +7,7 @@ FAILED_TEST="no"
 
 # good request
 RET1=$(curl -s -X POST -H "Content-Type: application/json" http://$IP:$PORT/api/dynupd -d \
-    '{"username": "alex", "password": "password", "ipaddr": "127.0.0.100", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
+    '{"username": "alex", "password": "password", "ipaddr": "192.0.2.4", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
 RET1CODE=$(echo $RET1 | jq -rM .code)
 if [ "$RET1CODE" != "200" ]; then
     echo "Test of good request failed."
@@ -15,16 +15,16 @@ if [ "$RET1CODE" != "200" ]; then
     FAILED_TEST="yes"
 else
     RETADDR=$(host host1.example.com $IP | head -1 | awk '{print $4}')
-    if [ "$RETADDR" != "127.0.0.100" ]; then
+    if [ "$RETADDR" != "192.0.2.4" ]; then
         FAILED_TEST="yes"
         echo "Test of host lookup on good request failed"
-        echo "Expected 127.0.0.100 but got: $RETADDR"
+        echo "Expected 192.0.2.4 but got: $RETADDR"
     fi
 fi
 
 # bad password
 RET2=$(curl -s -X POST -H "Content-Type: application/json" http://$IP:$PORT/api/dynupd -d \
-    '{"username": "alex", "password": "badpassword", "ipaddr": "127.0.0.100", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
+    '{"username": "alex", "password": "badpassword", "ipaddr": "192.0.2.4", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
 RET2CODE=$(echo $RET2 | jq -rM .code)
 if [ "$RET2CODE" != "403" ]; then
     echo "Test of bad password failed."
@@ -34,7 +34,7 @@ fi
 
 # bad username
 RET3=$(curl -s -X POST -H "Content-Type: application/json" http://$IP:$PORT/api/dynupd -d \
-    '{"username": "notalex", "password": "password", "ipaddr": "127.0.0.100", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
+    '{"username": "notalex", "password": "password", "ipaddr": "192.0.2.4", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
 RET3CODE=$(echo $RET3 | jq -rM .code)
 if [ "$RET3CODE" != "403" ]; then
     echo "Test of bad username failed."
@@ -44,7 +44,7 @@ fi
 
 # bad hosts
 RET4=$(curl -s -X POST -H "Content-Type: application/json" http://$IP:$PORT/api/dynupd -d \
-    '{"username": "alex", "password": "password", "ipaddr": "127.0.0.100", "hostnames": [ "host1", "badhost2" ], "version": "0.1.0"}')
+    '{"username": "alex", "password": "password", "ipaddr": "192.0.2.4", "hostnames": [ "host1", "badhost2" ], "version": "0.1.0"}')
 RET4CODE=$(echo $RET4 | jq -rM .code)
 if [ "$RET4CODE" != "418" ]; then
     echo "Test of bad hosts failed."
@@ -54,7 +54,7 @@ fi
 
 # malformed request - missing password
 RET5=$(curl -s -X POST -H "Content-Type: application/json" http://$IP:$PORT/api/dynupd -d \
-    '{"username": "alex", "ipaddr": "127.0.0.100", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
+    '{"username": "alex", "ipaddr": "192.0.2.4", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
 RET5CODE=$(echo $RET5 | jq -rM .code)
 if [ "$RET5CODE" != "400" ]; then
     echo "Test of malformed request - missing password failed."
@@ -74,7 +74,7 @@ fi
 
 # bad method - the PUT method seems like a resonable mistake people would make...
 RET7=$(curl -s -X PUT -H "Content-Type: application/json" http://$IP:$PORT/api/dynupd -d \
-    '{"username": "alex", "password": "password", "ipaddr": "127.0.0.100", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
+    '{"username": "alex", "password": "password", "ipaddr": "192.0.2.4", "hostnames": [ "host1", "host2" ], "version": "0.1.0"}')
 RET7CODE=$(echo $RET7 | jq -rM .code)
 if [ "$RET7CODE" != "405" ]; then
     echo "Test of bad method failed."
