a9cfd265e48b96ce8d6e82ec100aedc7ce65a68f
Alexander Necheff <alex@necheff.net>
2021-12-30 16:25:02 -0500

    added copyright and license to dynupd-broker test suite files

--- cmd/dynupd-broker/dynupdbroker_test.go
+++ cmd/dynupd-broker/dynupdbroker_test.go
@@ -1,3 +1,21 @@
+/*
+   Copyright (C) 2021 Alexander Necheff
+
+   This file is part of nsddyn.
+
+   nsddyn is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, at version 3 of the License.
+
+   nsddyn is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with nsddyn.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 package main_test
 
 import (
